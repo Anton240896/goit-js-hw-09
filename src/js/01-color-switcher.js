3bb48f4a81ace193e16d1706cf4fd06d21645964
Anton240896 <antalipov24081111111@gmail.com>
2023-08-14 17:05:31 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[ data-stop]');
+const body = document.querySelector('body');
 
+console.log(buttonStop);
+
+buttonStart.addEventListener('click', getChangeColorStart);
+buttonStop.addEventListener('click');
+
+function getChangeColorStart() {
+    body.style.backgroundColor = getChangeColor();
+}
+
+
+
+function getChangeColor() {
+    body.style.backgroundColor = getRandomHexColor();
+  
+     }
+
+
+           
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+        
