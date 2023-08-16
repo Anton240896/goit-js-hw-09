8211b929886f7213592f098b2eabdccdee2646d3
Anton240896 <antalipov24081111111@gmail.com>
2023-08-16 11:02:38 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,6 @@
 import getRandomHexColor from './getRandomColor';
 
+let color_random = null;
 
 const elem = {
 button_start : document.querySelector('button[data-start]'),
@@ -12,24 +13,21 @@ body : document.querySelector('body'),
 elem.button_start.addEventListener('click', onClickStart);
 elem.button_stop.addEventListener('click', onClickStop);
 
-let color_random = null;
-
-color_random = setTimeout(() => {
-  elem.body.style.backgroundColor = getRandomHexColor();
-}, 2000)
 
 function onClickStart() {
-  elem.body.style.backgroundColor = getRandomHexColor();
+  color_random = setInterval(() => {
+    elem.body.style.backgroundColor = getRandomHexColor();
+  },1000);
   elem.button_start.disabled = true;
   elem.button_stop.disabled = false;
-  
+
+
 }
+
 
 function onClickStop() {
-
   elem.button_start.disabled = false;
   elem.button_stop.disabled = true;
+
   clearInterval(color_random);
 }
-
-
