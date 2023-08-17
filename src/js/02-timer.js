852e26be32cf96ca48f5b9f5d58989a9410916c3
Anton240896 <antalipov24081111111@gmail.com>
2023-08-17 13:52:10 +0300

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,5 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import convertMs from './convertMs'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const elem = {
@@ -16,12 +15,52 @@ elem.button.disabled = true;
 
 let timerId = null;
 
-elem.button.addEventListener('click', clickStart);
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+     const selectedDate = (selectedDates[0]);
+     
+     //////////past
+     if (selectedDate < new Date()) {
+        Notify.failure("Please choose a date in the future");
+        elem.button.disabled = true;
 
+        /////////future
+      } else if (selectedDate > new Date()) {
+        elem.button.disabled = false;
+        Notify.success("Congratulations, you are now in the future");
+      }
+    }
+     };
+    
+  const flatpickr = flatpickr(elem.input, options);
+
+
+elem.button.addEventListener('click', clickStart);
 function clickStart() {
     timerId = setInterval(() => {
-let currentTime = new Date;
     })
 }
-let currentTime = Date.now;
-console.log(currentTime);
+
+
+ function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+  
+  function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
